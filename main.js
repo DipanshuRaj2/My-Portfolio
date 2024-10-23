d6041264ae793474be67259120568b27603a0852
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-10-23 17:20:29 +0530

    update portfolio

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-var tablinks =  document.getElementsByClassName("tab-links");
-var tabcontents  =  document.getElementsByClassName("tab-contents");
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzymFBZPV72nKFvtoNAVWMmLps6YbK0qqCeQvaKRYckbDv0C-DpQFIjTgGWauvhkJp87g/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('Form submitted!');  // Log when form is submitted
+    console.log(new FormData(form));  // Log the form data
 
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent to Dipanshu Successfully!!";
+            setTimeout(() => {
+                msg.innerHTML = "";
+            }, 1500);
+            form.reset();
+        })
+        .catch(error => {
+            msg.innerHTML = "Error sending message!";
+            console.error('Error!', error.message);
+        });
+});
